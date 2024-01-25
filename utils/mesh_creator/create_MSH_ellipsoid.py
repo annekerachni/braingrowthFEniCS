7af37496eac70b241a847cb381a538373b4c4abf
Anne Kerachni <89976599+annekerachni@users.noreply.github.com>
2024-01-25 14:36:33 +0100

    Update create_MSH_ellipsoid.py

--- utils/mesh_creator/create_MSH_ellipsoid.py
+++ utils/mesh_creator/create_MSH_ellipsoid.py
@@ -19,7 +19,7 @@ def create_MSH_ellipsoid_mesh(ellipsoid_output_path,
     # from http://en.wikipedia.org/wiki/Constructive_solid_geometry 
     # http://www.manpagez.com/info/gmsh/gmsh-2.2.6/gmsh_38.php
     # https://www.gmsh.info/doc/texinfo/gmsh.html#t11 
-    # https://www.gmsh.info/doc/texinfo/gmsh.html#Mesh-options !!!
+    # https://www.gmsh.info/doc/texinfo/gmsh.html#Mesh-options 
     gmsh.option.setNumber("Mesh.Algorithm3D", meshing_algorithm_number) # 3D mesh algorithm (1=Delaunay, 2=New Delaunay, 4=Frontal, 5=Frontal Delaunay, 6=Frontal Hex, 7=MMG3D, 9=R-tree)
     #gmsh.option.setNumber("Mesh.CharacteristicLengthExtendFromBoundary", 1)
     #gmsh.option.setNumber("Mesh.MeshSizeMin", tetra_size_min) # min size of tetrahedrons
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--spherecenter', help='Basis-sphere center C coordinates: (Cx, Cy, Cz)', type=tuple, required=False, default=(0.0, 0.0, 0.0))
     parser.add_argument('-dc', '--dilatationcoefficients', help='ellipsoid dilatation coefficients: (coef_x, coef_y, coef_z)', type=tuple, required=False, default=(1.0, 1.0, 1.0)) # sphere: (1.0, 1.0, 1.0), BrainGrowth ellipsoid: (0.9, 1.0, 0.7)
     parser.add_argument('-e', '--elementsize', help='Element (tetrahedron) size: (min, max)', type=float, required=False, default=(0.02, 0.05))
-    parser.add_argument('-o', '--output', help='Output path to write 3D sphere/ellipsoid (Gmsh object: .msh)', type=str, required=False, default='./data/gmsh/sphere_algoDelaunay1_tets005.msh' )
+    parser.add_argument('-o', '--output', help='Output path to write 3D sphere/ellipsoid (Gmsh object: .msh)', type=str, required=True, default='./data/gmsh/sphere_algoDelaunay1_tets005.msh' )
     parser.add_argument('-ma', '--meshingalgorithm', help='Meshing algorithm number', type=int, required=False, default=1) # 3D mesh algorithm (1=Delaunay, 2=New Delaunay, 4=Frontal, 5=Frontal Delaunay, 6=Frontal Hex, 7=MMG3D, 9=R-tree)
     parser.add_argument('-v', '--visualization', help='Plot creating mesh', type=bool, required=False, default=True)
 
