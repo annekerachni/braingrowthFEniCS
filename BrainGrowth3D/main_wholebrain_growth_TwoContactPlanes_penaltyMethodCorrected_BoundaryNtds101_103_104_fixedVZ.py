6bf6ee17331db64cb14c4b199c75264c99324f55
Anne Kerachni <89976599+annekerachni@users.noreply.github.com>
2024-12-20 15:23:33 +0100

    Update main_wholebrain_growth_TwoContactPlanes_penaltyMethodCorrected_BoundaryNtds101_103_104_fixedVZ.py

--- BrainGrowth3D/main_wholebrain_growth_TwoContactPlanes_penaltyMethodCorrected_BoundaryNtds101_103_104_fixedVZ.py
+++ BrainGrowth3D/main_wholebrain_growth_TwoContactPlanes_penaltyMethodCorrected_BoundaryNtds101_103_104_fixedVZ.py
@@ -1,4 +1,4 @@
-# elastodynamic brain growth model (dynamic structural mechanics)
+# FEniCS-based quasistatic brain growth model (purely solid continuum mechanics conservation law + penalization of contact between the two hemispheres)
 
 import sys, os
 os.environ['OMP_NUM_THREADS'] = '4'  # Set the number of OpenMP CPUs to use (the MUMPS linear solver, which is the FEniCS element based on PETSc using parallelization, is based on OpenMP)
@@ -27,7 +27,7 @@ from utils.converters import convert_meshformats
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='braingrowthFEniCS: brain growth elastodynamics 3D model')
+    parser = argparse.ArgumentParser(description='braingrowthFEniCS: brain growth quasistatic 3D model')
 
     parser.add_argument('-i', '--input', help='Input mesh path (.xml, .xdmf); mesh unit: either in millimeters, either in meters; mesh orientation: sagittal X, front X+, axial Y, front Y+, coronal Z, top Z+', type=str, required=False, 
                         default="./data/dHCP_surface_vs_volume_RAW/Vf/dhcp_surface_t21_raw_130000faces_455983tets_reoriented_dHCPVolume.xdmf") 
